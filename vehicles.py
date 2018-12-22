fe2f5bc9cf93bebfc70a23ff688915df6fc188f0
Faranio <40353497+Faranio@users.noreply.github.com>
2018-12-22 18:26:27 +0600

    Update vehicles.py

--- vehicles.py
+++ vehicles.py
@@ -6,6 +6,7 @@ class Car:
         self.cy = cy
         self.frameCount = frameCount
         self.track = [[cx, cy]]
+        self.fluctuations = 0
 
     def getID(self): #For the ID
         return self.pid
@@ -22,6 +23,9 @@ class Car:
     def getTrack(self):
         return self.track
 
+    def getFluctuations(self):
+        return self.fluctuations
+
     def updateCoords(self, cx, cy):
         self.track.append([cx, cy])
         self.cx = cx
@@ -30,5 +34,12 @@ class Car:
     def updateFrameCount(self, frameCount):
         self.frameCount = frameCount
 
+    def calculateFluctuations(self):
+        for i in range(len(self.track)):
+            if i == (len(self.track) - 1):
+                break
+            self.fluctuations += abs((self.track[i + 1][0] - self.track[i][0]))
+        return self.fluctuations
+
     def __repr__(self):
-        return "ID: {}, CX: {}, CY: {}, FrameCount: {}, Track: {}".format(self.pid, self.cx, self.cy, self.frameCount, self.track)
+        return "ID: {}, CX: {}, CY: {}, FrameCount: {}, Activity: {}".format(self.pid, self.cx, self.cy, self.frameCount, self.fluctuations)
