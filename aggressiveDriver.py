e0b077e1d65d80b8c43e5015dc503a43195dfec5
Faranio <40353497+Faranio@users.noreply.github.com>
2018-12-22 18:25:49 +0600

    Update aggressiveDriver.py

--- aggressiveDriver.py
+++ aggressiveDriver.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     n = 0
     pid = 0
-    #detectedCars = Set()
     cars = []
+    allCars = set([])
     frameCount = 0
 
     while (n != length):
@@ -22,16 +22,16 @@ if __name__ == "__main__":
         x1, y1 = 639, 178
         x2, y2 = 451, 719
         lineThickness = 2
-        cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
         x3, y3 = 727, 187
         x4, y4 = 790, 719
         lineThickness = 2
-        cv2.line(frame, (x3, y3), (x4, y4), (0, 0, 255), 2)        
+        cv2.line(frame, (x3, y3), (x4, y4), (0, 0, 255), 2)     
 
         gray = cv2.cvtColor(frame[180:], cv2.COLOR_BGR2GRAY)
 
-        objects = car_cascade.detectMultiScale(gray, 1.2, 5) #1.1, 3
+        objects = car_cascade.detectMultiScale(gray, 1.2, 5)
 
         for (x, y, w, h) in objects:
             y += 180
@@ -40,6 +40,7 @@ if __name__ == "__main__":
             if len(cars) == 0:
                 newCar = Car(pid, cx, cy, frameCount)
                 cars.append(newCar)
+                allCars.add(newCar)
                 pid += 1
                 continue
             for i in cars:
@@ -47,19 +48,19 @@ if __name__ == "__main__":
                     found = True
                     i.updateCoords(cx, cy)
                     i.updateFrameCount(frameCount)
+                    allCars.add(i)
                     break
             if not found:
                 newCar = Car(pid, cx, cy, frameCount)
                 cars.append(newCar)
+                allCars.add(newCar)
                 pid += 1
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-            print("Cars in frame #" + str(frameCount) + ": " + str(cars) + "\n")
 
         cars = [x for x in cars if not (frameCount - x.getFrameCount()) > 10]  
 
         for i in cars:
-            cv2.putText(frame, str(i.getID()), (int(i.getCX()), int(i.getCY())), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (randint(0, 255), randint(0, 255), randint(0, 255)), 1, cv2.LINE_AA)
+            cv2.putText(frame, str(i.getID()), (int(i.getCX()), int(i.getCY())), cv2.FONT_HERSHEY_SIMPLEX, 1, (randint(0, 255), randint(0, 255), randint(0, 255)), 1, cv2.LINE_AA)
                 
         cv2.imshow("Frame", frame)
         
@@ -67,6 +68,15 @@ if __name__ == "__main__":
             break
     
         n += 1
+
+    for i in allCars.copy():
+        i.calculateFluctuations()
+        if i.getFluctuations() == 0:
+            allCars.discard(i)
+            continue
+    
+    for i in sorted(allCars, key = Car.getFluctuations):
+        print(i)   
 
     print("Finished.")
     cap.release()
